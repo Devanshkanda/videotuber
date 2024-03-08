34ac5a00715d433420b96fef5e22ca3e839d0948
devanshkanda <devanshkanda02@gmail.com>
2024-03-08 18:12:09 +0530

    added foreign key relation in videos table

--- users/models.py
+++ users/models.py
@@ -1,72 +1,9 @@
 # from django.db import models
 from django.conf import settings
-import bson
+import bson, datetime
 from mongoengine import *
 from .utils import try_catch_wrapper
 
-# Create your models here.
-
-
-# class userDetails:
-#     collection = settings.DB['USERS']
-
-#     def __init__(
-#             self,
-#             fullName: str = None,
-#             username: str = None,
-#             email: str = None,
-#             password: str = None,
-#             avatar: str = None,
-#             coverImage: str = None,
-#             refreshToken = None,
-#             createdAt = None
-#             ) -> None:
-        
-#         self.fullName = fullName
-#         self.username = username
-#         self.email = email
-#         self.avatar = avatar
-#         self.coverImage = coverImage
-#         self.password = password
-#         self.refreshToken = refreshToken
-    
-
-#     @try_catch_wrapper
-#     def insert_details(self):
-#         userDetails.collection.insert_one(self.__dict__)
-#         print(f"Data inserted successfully")
-
-
-#     @try_catch_wrapper
-#     def fetch_all_details(self):
-#         gett = userDetails.collection.find()
-#         print(gett)
-#         print("fetched all details successfully")
-#         return gett
-    
-
-#     @try_catch_wrapper
-#     def fetch_Details_By_Id(self, id):
-#         userDetails.collection.find(bson.ObjectId(oid=id))
-#         print(f"FEtched user details successfully by id {id}")
-    
-
-#     @try_catch_wrapper
-#     def update_user_details(self, id, new_name):
-#         userDetails.collection.update_one(
-#             {"_id": bson.ObjectId(id)},
-#             {"$set": {
-#                 "name": new_name
-#             }}
-#         )
-    
-#     @try_catch_wrapper
-#     def delete_video(self, id):
-#         userDetails.collection.delete_one({"_id": bson.ObjectId(oid=id)})
-
-
-
-import datetime
 
 class userDetails(Document):
     username = StringField(max_length=20, required=True, unique=True)
@@ -76,6 +13,7 @@ class userDetails(Document):
     avatar = StringField() # url of file stored on cloudinary
     coverImage = StringField() # url of file stored on cloudinary
     created_at = DateTimeField(default=datetime.datetime.utcnow())
+    updated_at = DateTimeField(default=datetime.datetime.utcnow())
     refreshtoken = StringField()
 
 
@@ -87,6 +25,6 @@ class videos(Document):
     duration = IntField(default=0)
     views = fields.Int64()
     isPublished = BooleanField(default=False)
-    owner = ObjectIdField(required=True)
+    owner = ReferenceField(userDetails, reverse_delete_rule=CASCADE) # a foreign key refering to userDetails table
     created_at = DateTimeField(default=datetime.datetime.utcnow())
     updated_at = DateTimeField(default=datetime.datetime.utcnow())
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from rest_framework import routers
+
 
 urlpatterns = [
     path('register/', get_details, name="get_details")
